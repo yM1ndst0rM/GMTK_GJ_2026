a5022b9b2617391bd7a495c68125bce9a12f936b
yM1ndst0rM <ym1ndst0rm@users.noreply.github.com>
2026-07-26 04:09:04 +0200

    Add villager effect script

--- scripts/villager_controller.gd
+++ scripts/villager_controller.gd
@@ -1,16 +1,10 @@
 extends Node
 class_name VillagerController
 
-signal villager_died(cell: Vector2i) 
-
 func get_all_villagers()-> Array[Vector2i]:
 	return CellsTypes.get_all_cells_of_type($InteractablesLayer , CellsTypes.T_VILLAGER)
 
 func kill_villager_on(cell: Vector2i):
 	if(CellsTypes.is_cell_of_type($InteractablesLayer, cell, CellsTypes.T_VILLAGER)):
-		_villager_died_effect(cell)
-		villager_died.emit($InteractablesLayer, cell)
+		EventBus.villager_killed.emit(cell, get_all_villagers().size())
 		
-
-func _villager_died_effect(cell: Vector2i):
-	$InteractablesLayer.erase_cell(cell)
