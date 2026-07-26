4a59be257d6587a158d7bb465e7609ac76598c82
yM1ndst0rM <ym1ndst0rm@users.noreply.github.com>
2026-07-26 03:49:17 +0200

    Add rudimentary event bus class

---
+++ scripts/event_bus.gd
@@ -0,0 +1,25 @@
+extends Node
+
+## Global event bus
+# Only events, that are of interest to many different parts of the game should be exposed here
+# This script will be autoloaded so every component can subscribe to and unsubscribe from all events of interest
+# Naming convention:
+# Try to be as clear as possible, what kind of action triggered the signal, so that it is clear, who
+# should react to it and why.
+
+
+
+#game events
+signal game_started
+signal game_ended(win: bool)
+
+#player events
+signal snake_moved(old_head_cell: Vector2i, head_cell: Vector2i)
+signal snake_captured_area_changed(captured_cells: Array[Vector2i])
+signal snake_health_changed(old_health: int, health: int)
+
+#interactables
+signal interaction_triggered_health_change(health_change: int)
+
+#villager events
+signal villager_killed(villager_location_cell: Vector2i, number_of_remaining_villagers: int)
